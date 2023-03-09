97450a6bcb73fa910fb676d5ac594ec27c632ff2
Matheus Brian de Oliveira <80044011+matheusbriandeoliveira@users.noreply.github.com>
2023-03-09 15:37:56 -0300

    atualizando

---
+++ juntando-salas.js
@@ -0,0 +1,5 @@
+const salaJS = ["Evaldo", "Camis", "Mari"];
+const salaPython = ["Ju", "Leo", "Raquel"];
+
+const salas = salaJS.concat(salaPython);
+console.log(salas);
---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,7 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [0, 8, 7.5, 9];
+
+const listaJuntas = [alunos, medias];
+
+console.log(`A aluna da posição 1 é ${listaJuntas[0][1]}. A nota dessa aluna é ${listaJuntas[1][1]}`);
+
