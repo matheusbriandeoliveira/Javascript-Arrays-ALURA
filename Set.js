5fc5fd5400af49e3c24f27d11f70aaf25772dc88
Matheus Brian de Oliveira <80044011+matheusbriandeoliveira@users.noreply.github.com>
2023-03-22 19:40:04 -0300

    finalizacao

---
+++ Set.js
@@ -0,0 +1,9 @@
+const nomes = ["Ana", "Clara", "Maria", "Maria", "João", "João", "João"];
+
+const meuSet = new Set(nomes);
+
+
+const nomesAtualizados = [...meuSet];
+    
+    
+console.log(nomesAtualizados);
